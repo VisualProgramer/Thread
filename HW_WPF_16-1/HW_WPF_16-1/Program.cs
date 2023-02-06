c8ed178b905145df36ff390b053da23864c0efb1
VisualProgramer <potalitcin@gmail.com>
2023-02-05 21:36:28 -0500

    Added first task

---
+++ HW_WPF_16-1/HW_WPF_16-1/Program.cs
@@ -0,0 +1,35 @@
+﻿//How needed set Name for Threads?
+
+File.Delete("MainFile.json");
+
+int filesCount = Directory.GetFiles("Files", "*", SearchOption.AllDirectories).Length;
+int fileNumber = 0;
+string dataFromFiles = "";
+Thread[] threads = new Thread[filesCount];
+Thread thread = new Thread(ReadFromFiles);
+
+for (int i = 0; i < filesCount; i++)
+{
+    threads[i] = new Thread(ReadFromFile);
+}
+
+thread.Start();
+
+
+//=============================================================//
+void ReadFromFiles()
+{
+    for (int i = 0; i < filesCount; i++)
+    {
+        threads[i].Start();
+        threads[i].Join();
+    }
+}
+
+void ReadFromFile()
+{
+    dataFromFiles = File.ReadAllText($"Files\\File{fileNumber}.json");
+    fileNumber++;
+    File.AppendAllText("MainFile.json", dataFromFiles);
+}
+
