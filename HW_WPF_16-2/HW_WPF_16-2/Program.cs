6913f2dcc62ee96dae7a3dd93aed9c1c5af1f872
VisualProgramer <potalitcin@gmail.com>
2023-02-05 21:59:18 -0500

    Add second task

---
+++ HW_WPF_16-2/HW_WPF_16-2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Используя конструкции блокировки, создайте метод, который будет в цикле for (допустим, на 10 итераций) увеличивать счетчик на единицу и выводить на экран счетчик и текущий поток. 
+//Метод запускается в трех потоках. Каждый поток должен выполниться поочередно, т.е. в результате на экран должны выводиться числа (значения счетчика) с 1 до 30 по порядку, а не в произвольном порядке.
+
+int count = 0;
+
+var th1 = new Thread(Counter);
+var th2 = new Thread(Counter);
+var th3 = new Thread(Counter);
+
+th1.Start(); th1.Join();
+th2.Start(); th2.Join();
+th3.Start(); th3.Join();
+
+void Counter()
+{
+    for (int i = 0; i < 10; i++)
+        Console.WriteLine("count = " + ++count + " Thread - " + Thread.GetCurrentProcessorId());
+}
